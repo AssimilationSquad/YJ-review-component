a366c3974134b92f0b8db88e807fdaf2bf1ce7f3
Yuki Jung <yukiej@gmail.com>
2018-10-29 14:52:58 -0700

    Add route for PATCH request when a review is flagged and GET request when user filters by search term

--- server/index.js
+++ server/index.js
@@ -7,15 +7,32 @@ const helpers = require('./queryHelpers.js');
 const app = express();
 const port = 3002;
 
+app.use(express.static('../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('../public'));
+
 
 app.get('/home/:homeid/reviews', (req, res) => {
   const homeID = req.params.homeid;
+  const keyword = req.query.keyword || undefined;
   // get data from database
-  helpers.getReviews(homeID, (results) => {
-    res.status(200).send(results);
+  if (keyword === undefined) {
+    helpers.getReviews(homeID, (results) => {
+      res.status(200).send(results);
+    });
+  } else {
+    helpers.searchReviews(homeID, keyword, (results) => {
+      res.status(200).send(results);
+    });
+  }
+});
+
+app.patch('/home/:homeid/reviews/:reviewid', (req, res) => {
+  const reviewID = req.params.reviewid;
+  // update database
+  helpers.updateFlags(reviewID, () => {
+    console.log('Flag added to review: ', reviewID);
+    res.status(200).send("flag recorded!");
   });
 });
 
