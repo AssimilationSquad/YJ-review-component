32f043b5d47ba676cfecf190c85a3d7499f59507
Yuki Jung <yukiej@gmail.com>
2018-10-30 11:09:30 -0700

    Edits based on Kent's most excellent feedback

--- server/index.js
+++ server/index.js
@@ -1,29 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('../database/index.js');
 const helpers = require('./queryHelpers.js');
 
 const app = express();
 const port = 3002;
 
 // app.use(express.static('../public'));
-app.use(express.static(`${__dirname}/../client/dist/`));
+app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/rooms/:homeid/reviews', (req, res) => {
   const homeID = req.params.homeid;
-  const keyword = req.query.keyword || undefined;
+  const { keyword } = req.query;
   // get data from database
   if (keyword === undefined) {
-    helpers.getReviews(homeID, (results) => {
-      res.status(200).send(results);
+    helpers.getReviews(homeID, (err, results) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send(results);
+      }
     });
   } else {
-    helpers.searchReviews(homeID, keyword, (results) => {
-      res.status(200).send(results);
+    helpers.searchReviews(homeID, keyword, (err, results) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send(results);
+      }
     });
   }
 });
@@ -31,9 +38,12 @@ app.get('/rooms/:homeid/reviews', (req, res) => {
 app.patch('/rooms/:homeid/reviews/:reviewid', (req, res) => {
   const reviewID = req.params.reviewid;
   // update database
-  helpers.updateFlags(reviewID, () => {
-    console.log('Flag added to review: ', reviewID);
-    res.status(200).send("flag recorded!");
+  helpers.updateFlags(reviewID, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      res.status(200).send('flag recorded!');
+    }
   });
 });
 
