b8dcbae08a2c189772a823c07c1cf8bf506f6cce
Yuki Jung <yukiej@gmail.com>
2018-10-29 17:44:58 -0700

    Making skeleton for React components

---
+++ client/src/components/ReviewListEntry.jsx
@@ -0,0 +1,16 @@
+ const React = require('react');
+
+class ReviewListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>This is one review</h3>
+      </div>
+    )
+  }
+}
+
+module.exports = ReviewListEntry;
