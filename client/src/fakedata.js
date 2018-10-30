2f9b63de80205bc3899e8889c3ef7ae24ce37072
Yuki Jung <yukiej@gmail.com>
2018-10-30 09:27:22 -0700

    Some fake data to use for developing React components

---
+++ client/src/fakedata.js
@@ -0,0 +1,13 @@
+const sampleReviews = [{
+ 'Review_id': 5, First_name: 'Krista', Review_date: '2017-10-02T07:00:00.000Z', Body: 'Et deleniti omnis optio voluptatem inventore occaecati. Saepe quo dolor. Illo consequatur nesciunt non sit reprehenderit placeat qui similique ex. Est ut distinctio.', Stars: 3, Thumbnail_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/commadelimited/128.jpg' 
+}, {
+ 'Review_id': 3, 'First_name': 'Aidan', 'Review_date': '2017-06-04T07:00:00.000Z', Body: 'Harum eius quo recusandae est. Culpa soluta impedit beatae et enim. Velit qui iure itaque id error at earum. Nisi necessitatibus blanditiis odit dolor nihil reiciendis magnam ipsam praesentium.', Stars: 3, Thumbnail_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/olaolusoga/128.jpg' 
+}, {
+ 'Review_id': 2, 'First_name': 'Deangelo', Review_date: '2017-01-29T08:00:00.000Z', Body: 'Enim ut corrupti itaque perferendis culpa dicta illo repudiandae. In modi rem ipsam aut sint. Voluptatem consequatur eum. Sequi dignissimos eius harum inventore quasi rerum sit facere reiciendis. Consectetur suscipit architecto et tempore rerum aut quae. Aut accusantium modi voluptas explicabo velit ratione non quia quas.', 'Stars': 5, Thumbnail_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/danmartin70/128.jpg' 
+}, {
+ 'Review_id': 1, First_name: 'Marquise', 'Review_date': '2015-03-21T07:00:00.000Z', 'Body': 'Officiis fuga nemo. Labore amet quibusdam autem excepturi voluptas eligendi aut fuga rerum. Rerum ipsum animi iste unde et. Cumque possimus doloribus. Doloremque reiciendis nulla.', Stars: 5, 'Thumbnail_url': 'https://s3.amazonaws.com/uifaces/faces/twitter/barputro/128.jpg' 
+}, { 
+Review_id: 4, First_name: 'Isidro', 'Review_date': '2015-02-24T08:00:00.000Z', Body: 'Perferendis doloremque magnam voluptas voluptates cum incidunt expedita ipsam perferendis. Sint vitae neque ullam velit nihil corrupti molestiae accusamus. Exercitationem eum asperiores impedit est aliquam animi nobis.', Stars: 5, 'Thumbnail_url': 'https://s3.amazonaws.com/uifaces/faces/twitter/lewisainslie/128.jpg' 
+}];
+
+module.exports = sampleReviews;
