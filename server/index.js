f4e21900258414a37455e8059e43f32666aadd3f
Yuki Jung <yukiej@gmail.com>
2018-10-30 17:10:07 -0700

    Using path.join for path to static directory

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3002;
 
 // app.use(express.static('../public'));
-app.use(express.static(`${__dirname}/../client/dist/`));
+app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
