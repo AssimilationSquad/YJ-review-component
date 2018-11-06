96a16ba7334ffbee1bcc769994b001e40c305c8a
Yuki Jung <yukiej@gmail.com>
2018-11-05 19:07:53 -0800

    Change div id to avoid having 4 components with id="app"
    Also made .eslintrc file to specify global variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
-  extends: 'airbnb-base'
+    "extends": "airbnb", 
+    "env": {
+        "browser": true
+    }
 };
