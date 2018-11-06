417e37fefadbb5cda4adb3be2f060652febca9b2
Yuki Jung <yukiej@gmail.com>
2018-11-06 10:25:00 -0800

    Add max-width inline for reviews component

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('../database/index.js');
 const helpers = require('./queryHelpers.js');
 
 const app = express();
