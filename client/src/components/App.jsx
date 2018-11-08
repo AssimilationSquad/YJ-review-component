d12e5861e9a0fbce7dccf366a8a2247c0909429b
Yuki Jung <yukiej@gmail.com>
2018-11-07 17:32:11 -0800

    Resolving some linter errors

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ReviewList from './ReviewList.jsx';
 import StarRatings from './StarRatings.jsx';
@@ -12,62 +13,69 @@ class App extends React.Component {
       filter: undefined,
       filtered_reviews: [],
       active_reviews: [],
-    }
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleReadMore = this.handleReadMore.bind(this);
   }
 
   componentDidMount() {
-    let url = window.location.href + '/reviews';
+    const url = `${window.location.href}/reviews`;
     fetch(url, {
-      method: 'GET', 
+      method: 'GET',
     })
-    .then((res) => {
-      return res.json();
-    })
-    .then((body) => {
-      this.setState({
-        reviews: body.reviews,
-        ratings: body.ratings,
-      })
-    })
+      .then(res => res.json())
+      .then((body) => {
+        this.setState({
+          reviews: body.reviews,
+          ratings: body.ratings,
+        });
+      });
   }
 
   handleSearch(event) {
-    let searchTerm = event.target.value;
-    let filteredRev = this.state.reviews.filter(rev => rev.Body.toLowerCase().includes(searchTerm.toLowerCase()));
+    const searchTerm = event.target.value;
+    const filteredRev = this.state.reviews.filter(rev => rev.Body.toLowerCase().includes(searchTerm.toLowerCase()));
     this.setState({
       filter: searchTerm,
       filtered_reviews: filteredRev,
-    })
+    });
   }
 
   handleReadMore(id) {
-    let updated = this.state.active_reviews.concat([Number(id)]);
+    const updated = this.state.active_reviews.concat([Number(id)]);
     this.setState({
       active_reviews: updated,
-    })
+    });
   }
 
   render() {
     if (this.state.filtered_reviews.length === 0 || this.state.filter === '') {
       return (
         <div className="app">
-          <StarRatings ratings={this.state.ratings} numReviews={this.state.reviews.length} handler={this.handleSearch.bind(this)}/>
-          <ReviewList reviews={this.state.reviews} expanded={this.state.active_reviews} handleExpand={this.handleReadMore.bind(this)}/>
+          <StarRatings ratings={this.state.ratings} numReviews={this.state.reviews.length} handler={this.handleSearch} />
+          <ReviewList reviews={this.state.reviews} expanded={this.state.active_reviews} handleExpand={this.handleReadMore} />
         </div>
-      )
-    } else {
-      let message = ' guests have mentioned ';
-      if (this.state.filtered_reviews.length === 1) {
-        message = ' guest has mentioned ';
-      }
-      return (
-        <div className="app">
-          <StarRatings ratings={this.state.ratings} numReviews={this.state.reviews.length} handler={this.handleSearch.bind(this)}/>
-          <p className="filter" className={styles.filter}>{this.state.filtered_reviews.length} {message} "{this.state.filter}" </p>
-          <ReviewList reviews={this.state.filtered_reviews} expanded={this.state.active_reviews} handleExpand={this.handleReadMore.bind(this)}/>
-        </div>
-      )
+      );
     }
+    let message = ' guests have mentioned ';
+    if (this.state.filtered_reviews.length === 1) {
+      message = ' guest has mentioned ';
+    }
+    return (
+      <div className="app">
+        <StarRatings ratings={this.state.ratings} numReviews={this.state.reviews.length} handler={this.handleSearch} />
+        <p className={styles.filter}>
+          {this.state.filtered_reviews.length}
+          {' '}
+          {message}
+          {' '}
+"
+          {this.state.filter}
+">
+                </p>
+        <ReviewList reviews={this.state.filtered_reviews} expanded={this.state.active_reviews} handleExpand={this.handleReadMore} />
+      </div>
+    );
   }
 }
 
